9360f082839da1ce46c4e259aef48b2bc9cdba88
fady <gamilfady605@gmail.com>
2022-09-14 12:41:03 +0200

    Update Profile is done

--- SehaNotebook.API/Controllers/V1/ProfileController.cs
+++ SehaNotebook.API/Controllers/V1/ProfileController.cs
@@ -3,17 +3,18 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using SehaNotebook.DAL.IConfiguration;
 using Microsoft.AspNetCore.Identity;
+using SehaNotebook.Domain.DTOs;
 
 namespace SehaNotebook.API.Controllers.V1
 {
-   [Route("api/v{version:apiVersion}/")]
+   [Route("api/v{version:apiVersion}/profile")]
    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme )]
    public class ProfileController : BaseController
    {
       public ProfileController(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager) : base(unitOfWork, userManager)
       {   }
       
-      [HttpGet("profile")]
+      [HttpGet("")]
       public async Task<IActionResult> GetProfile ()
       {
          /*
@@ -23,7 +24,7 @@ namespace SehaNotebook.API.Controllers.V1
          var loggedInUser = await _userManager.GetUserAsync(HttpContext.User);
 
          if (loggedInUser == null)
-            return BadRequest("User is not found");
+            return NotFound("User is not found");
 
          // after making sure that this user exists, we need to retrieve its info from DB [our USERS table]
          var Profile = await _unitOfWork.UserRepository.GetProfileByIdentityId(new Guid(loggedInUser.Id));
@@ -36,6 +37,35 @@ namespace SehaNotebook.API.Controllers.V1
          {
             return Ok(Profile);
          }
+      }
+
+      [HttpPut("")]
+      public async Task<IActionResult> UpdateProfile([FromBody] UpdateUserDto dto)
+      {
+         var loggedInUser = await _userManager.GetUserAsync(HttpContext.User);
+
+         if (loggedInUser == null) return NotFound();
+
+         var profile = await _unitOfWork.UserRepository.GetProfileByIdentityId(new Guid(loggedInUser.Id));
+
+         if(profile == null) return NotFound();
+         if (dto.Address != null) 
+            profile.Address = dto.Address;
+         if (dto.Phone != null)
+            profile.Phone = dto.Phone;
+         if (dto.Country != null)
+            profile.Country = dto.Country;
+         if (dto.Sex != null)
+            profile.Sex = dto.Sex;
+
+         // update the info inside the DB
+         var updateResult = await _unitOfWork.UserRepository.UpdateProfileInfo(profile);
+         if (updateResult == true) 
+         {
+            await _unitOfWork.CompleteAsyncOperations();
+            return Ok(profile);
+         }
+         else return StatusCode(500, "Error while updating your inforimations");
       }
    }
 }
---
+++ SehaNotebook.Domain/DTOs/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+namespace SehaNotebook.Domain.DTOs
+{
+   public class UpdateUserDto
+   {
+      public string Phone {get; set;}
+      public string Country {get; set;}
+      public string Sex {get; set;}
+      public string Address {get; set;}
+   }
+}
